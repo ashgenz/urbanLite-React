e0a8581e23dce3b41b55cee58ce9f7c6dc6a0ac8
ashgenz <as9875147324@gmail.com>
2025-07-16 05:41:59 +0530

    hn

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,6 @@
 // vite.config.mjs
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
