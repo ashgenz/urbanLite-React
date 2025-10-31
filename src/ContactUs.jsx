f93917d8a717599b26583223d8d96bbc5cdd71b3
ashgenz <as9875147324@gmail.com>
2025-10-31 06:07:42 +0530

    d

--- src/ContactUs.jsx
+++ src/ContactUs.jsx
@@ -66,7 +66,6 @@ export default function ContactUs({ LoggedIn}) { // optionally pass name
             <label className="block text-gray-700 mb-1">Name</label>
             <input
               type="text"
-              value={userName || ""}
               className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500 outline-none bg-gray-100 cursor-not-allowed"
               disabled
             />
