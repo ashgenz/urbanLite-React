03735f54c21742bf3a0e8a659cb1d995d84fbb35
ashgenz <as9875147324@gmail.com>
2025-12-08 15:00:54 +0530

    1699 cook price

--- src/priceConfig.js
+++ src/priceConfig.js
@@ -1,5 +1,5 @@
 // priceConfig.js
 export const UNIT_PRICES = {
-  Monthly: { room: 8, kitchen: 10, hall: 12, toilet: 15, meal: 25, naashta: 15, bartan: 1 },
+  Monthly: { room: 8, kitchen: 10, hall: 12, toilet: 15, meal: 28.31, naashta: 15, bartan: 1 },
   OneTime: { room: 10, kitchen: 13, hall: 15, toilet: 18, meal: 40,naashta: 20, bartan: 2 },
 };
